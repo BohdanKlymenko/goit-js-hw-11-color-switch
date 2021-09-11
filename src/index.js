f5d0a7a9f176260f40ef5c6be94097b58bd1e36b
BohdanKlymenko <81781697+BohdanKlymenko@users.noreply.github.com>
2021-09-11 19:33:59 +0300

    task 1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
 import './sass/main.scss';
+import { getRefs } from "./js/getRefs"
+const refs = getRefs
+
+const colors = [
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+];
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+
+// const bodyRef = document.querySelector('body')
+// const startRef = document.querySelector('[data-action="start"]')
+// const stopRef = document.querySelector('[data-action="stop"]')
+let intervalID = null;
+
+
+const bodyColor = ()=> {refs.body.style.backgroundColor = colors[randomIntegerFromInterval(0, colors.length - 1)];}
+
+refs.startBtn.addEventListener('click', () => {
+    intervalID = setInterval(bodyColor, 1000);
+refs.startBtn.disabled= true})
+
+refs.stopBtn.addEventListener('click', () => {
+    clearInterval(intervalID)
+    // bodyRef.style.backgroundColor = 'silver'
+    refs.stopBtn.disabled = false
+})
