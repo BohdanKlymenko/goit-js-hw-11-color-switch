7f4d2e5387f297294c9b8fffb39a5e1ebcb5eb9d
BohdanKlymenko <81781697+BohdanKlymenko@users.noreply.github.com>
2021-09-21 21:00:24 +0300

    форматирует код

--- src/index.js
+++ src/index.js
@@ -1,15 +1,8 @@
 import './sass/main.scss';
-import { getRefs } from "./js/getRefs"
+import { getRefs } from './js/getRefs';
 const refs = getRefs();
 
-const colors = [
-  '#FFFFFF',
-  '#2196F3',
-  '#4CAF50',
-  '#FF9800',
-  '#009688',
-  '#795548',
-];
+const colors = ['#FFFFFF', '#2196F3', '#4CAF50', '#FF9800', '#009688', '#795548'];
 
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
@@ -17,13 +10,16 @@ const randomIntegerFromInterval = (min, max) => {
 
 let intervalID = null;
 
-const bodyColor = ()=> {refs.body.style.backgroundColor = colors[randomIntegerFromInterval(0, colors.length - 1)];}
+const bodyColor = () => {
+  refs.body.style.backgroundColor = colors[randomIntegerFromInterval(0, colors.length - 1)];
+};
 
 refs.startBtn.addEventListener('click', () => {
-    intervalID = setInterval(bodyColor, 1000);
-refs.startBtn.disabled= true})
+  intervalID = setInterval(bodyColor, 1000);
+  refs.startBtn.disabled = true;
+});
 
 refs.stopBtn.addEventListener('click', () => {
-    clearInterval(intervalID)
-    refs.startBtn.disabled = false
-})
+  clearInterval(intervalID);
+  refs.startBtn.disabled = false;
+});
